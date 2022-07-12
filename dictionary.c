e2ab81c302f13d6ce57f7f96ad191e48adb663f7
Franco <fgozzerino@gmail.com>
2022-07-12 15:00:37 -0300

    Primeros acercamientos de tabla hash

--- dictionary.c
+++ dictionary.c
@@ -1,60 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tablahash.h"
+#include "dictionary.h"
 #define HASHSIZE 620909
-
-
-typedef struct dict_entry_s {
-    const char *key;
-    int value;
-} dict_entry_s;
-
-typedef struct dict_s {
-    int len;
-    int cap;
-    dict_entry_s *entry;
-} dict_s, *dict_t;
-
-int dict_find_index(dict_t dict, const char *key) {
-    for (int i = 0; i < dict->len; i++) {
-        if (!strcmp(dict->entry[i], key)) {
-            return i;
-        }
-    }
-    return -1;
+dict_entry_s *create_entry(char *key)
+{
+    dict_entry_s *entry = malloc(sizeof(dict_entry_s));
+    entry->key = malloc(sizeof(char) * strlen(key) + 1);
+    strcpy(entry->key, key);
+    entry->hash = dict_hash(key);
+    return entry;
+}
+unsigned dict_hash(char *key)
+{
+    return KRHash(key);
+}
+void dict_destroy(dict_entry_s *entry)
+{
+    free(entry->key);
+    free(entry);
+}
+int dict_cmp(dict_entry_s *entry1, char *key)
+{
+    return strcmp(entry1->key, key);
+}
+dict_entry_s *dict_copy(dict_entry_s *entry)
+{
+    dict_entry_s *copy = create_entry(entry->key);
+    return copy;
 }
 
-int dict_find(dict_t dict, const char *key, int def) {
-    int idx = dict_find_index(dict, key);
-    return idx == -1 ? def : dict->entry[idx].value;
+int find_dict(TablaHash table, char *key)
+{
+    return tablahash_buscar(table, key)) ? 1 : 0 ;
 }
-
-void dict_add(dict_t dict, const char *key, int value) {
-   int idx = dict_find_index(dict, key);
-   if (idx != -1) {
-       dict->entry[idx].value = value;
-       return;
-   }
-   if (dict->len == dict->cap) {
-       dict->cap *= 2;
-       dict->entry = realloc(dict->entry, dict->cap * sizeof(dict_entry_s));
-   }
-   dict->entry[dict->len].key = strdup(key);
-   dict->entry[dict->len].value = value;
-   dict->len++;
-}
-
-dict_t dict_new(void) {
-    dict_s proto = {0, 10, malloc(10 * sizeof(dict_entry_s))};
-    dict_t d = malloc(sizeof(dict_s));
-    *d = proto;
-    return d;
-}
-
-void dict_free(dict_t dict) {
-    for (int i = 0; i < dict->len; i++) {
-        free(dict->entry[i].key);
-    }
-    free(dict->entry);
-    free(dict);
+unsigned hash_dict();
+void add_dict();
+void destroy_dict();
+TablaHash new_dict()
+{
+    return tablahash_crear(HASHSIZE, (FuncionCopiadora)dict_copy, (FuncionComparadora)dict_cmp, (FuncionDestructora)dict_destroy, (FuncionHash)dict_hash);
 }
--- dictionary.h
+++ dictionary.h
@@ -1,10 +1,19 @@
 #ifndef __DICTIONARY_H_
 #define __DICTIONARY_H_
 
-void find_dict();
-void hash_dict();
-void add_dict();
+typedef struct dict_entry_s
+{
+    const char *key;
+    int hash;
+} dict_entry_s;
+
+unsigned hash_dict();
 void destroy_dict();
+int cmp();
+dict_entry_s *copy();
+
+void add_dict();
 void new_dict();
+void find_dict();
 
 #endif /* __DICTIONARY_H_ */
